76d8680e5f6c082862b6b44a2521feef167a0e02
Kevin Quinn <quinnk@scc1.bu.edu>
2024-12-26 15:45:24 -0500

    worst bloc experiment

--- experiments/worst_bloc_example.py
+++ experiments/worst_bloc_example.py
@@ -4,7 +4,7 @@ import math
 from metric_voting import *
 
 np.random.seed(918717)
-filename = 'data/2bloc_worst_example2.npz'
+filename = 'data/2bloc_worst_example3.npz'
 
 # Choose number of voters n and the number of candidates m
 n = 1000
